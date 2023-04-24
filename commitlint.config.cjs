2c3c4115df9ed7f2a9082073b2553a0e75364611
Ilya Marchenko <work.marchenko.i@gmail.com>
2023-04-24 20:49:58 +0300

    build(commitlint): Ignore subject-case rule for commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,14 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
+    rules: {
+        'subject-case': [
+            2,
+            'never',
+            [
+                'upper-case',
+                'pascal-case',
+                'start-case',
+            ]
+        ]
+    }
 };
